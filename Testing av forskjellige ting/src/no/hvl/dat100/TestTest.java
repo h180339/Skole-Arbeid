cf58e82d3cbfe23523a9412acda8faff8c90260d
eirik <eirik@DESKTOP-OQVI56F>
2017-09-08 12:20:23 +0200

    TEST TEST

---
+++ Testing av forskjellige ting/src/no/hvl/dat100/TestTest.java
@@ -0,0 +1,13 @@
+package no.hvl.dat100;
+
+public class TestTest {
+
+	public static void main(String[] args) {
+		 int [] arry = new int [7];
+		 arry [0] = (int)(Math.random()*100);
+		 
+		 System.out.print(arry [0]);
+
+	}
+
+}
